82ea493cbebb793671e92b3edbd4ac779d9aced3
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-21 10:03:27 +0400

    add main apps in project

---
+++ kurs_service/site_login/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SiteLoginConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'site_login'
---
+++ kurs_service/site_signup/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SiteSignupConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'site_signup'
