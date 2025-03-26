9b2e039bbc17106a0184cada9ffabb2802323a71
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-26 18:57:07 +0400

    add check_list html

--- kurs_service/check_list/views.py
+++ kurs_service/check_list/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def check_list(request):
     context = {
-        'title': 'Чен лист | OOO &quot;КУРС&quot;',
+        'title': 'Чен лист | OOO "КУРС"',
     }
     return render(request, "check_list/check_list.html", context=context)
