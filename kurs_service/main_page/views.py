806f0eba1d0385a3aa1b772c03edbdb9a6e6d16f
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-26 18:59:31 +0400

    add main_page html

--- kurs_service/main_page/views.py
+++ kurs_service/main_page/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def main_page(request):
     context = {
-        'title': 'Главная страница | OOO &quot;КУРС&quot;',
+        'title': 'Главная страница | OOO "КУРС"',
     }
     return render(request, "main_page/main_page.html", context=context)
