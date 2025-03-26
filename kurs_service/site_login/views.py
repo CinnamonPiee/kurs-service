cd4b558501a14bca9c24a716db8a66e1cd09a599
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-26 19:02:14 +0400

    add site_login html

--- kurs_service/site_login/views.py
+++ kurs_service/site_login/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def site_login(request):
     context = {
-        'title': 'Вход | OOO &quot;КУРС&quot;',
+        'title': 'Вход | OOO "КУРС"',
     }
     return render(request, "site_login/site_login.html", context=context)
