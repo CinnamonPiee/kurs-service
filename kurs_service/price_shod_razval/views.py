d9bc15ba2466c3f73f6127f865afd8598ae23115
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-26 19:01:20 +0400

    add price_shod_razval html

--- kurs_service/price_shod_razval/views.py
+++ kurs_service/price_shod_razval/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def price_shod_razval(request):
     context = {
-        'title': 'Прайс развал-схождение | OOO &quot;КУРС&quot;',
+        'title': 'Прайс развал-схождение | OOO "КУРС"',
     }
     return render(request, "price_shod_razval/price_shod_razval.html", context=context)
