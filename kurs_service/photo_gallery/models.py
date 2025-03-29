c67490e6dca22eeae0660cfebce9a9e644f74e3e
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-29 15:22:05 +0400

    add models and migrations

--- kurs_service/photo_gallery/models.py
+++ kurs_service/photo_gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class PhotoGallery(models.Model):
+    description = models.CharField(max_length=255, blank=True, null=True)
+    photo_name = models.CharField(max_length=255, blank=True, null=True)
+    photo_name_min = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.description if self.description else "Фото"
+
+    class Meta:
+        db_table = "photo_gallery"
