619222cc1ed2cf7389b035e29dd744a70c54b706
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-29 15:22:56 +0400

    add models and migrations

--- kurs_service/warranty/models.py
+++ kurs_service/warranty/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class WarrantyObligations(models.Model):
+    general_provisions = models.TextField(blank=True, null=True)
+    warranty_period = models.TextField(blank=True, null=True)
+    execution_conditions = models.TextField(blank=True, null=True)
+    cancellation_conditions = models.TextField(blank=True, null=True)
+    list_types_work = models.TextField(blank=True, null=True)
+    parking_conditions = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.general_provisions[:50]  # Возвращает первые 50 символов общего положения для отображения
+
+    class Meta:
+        db_table = "warranty_obligations"
