0300c5147a5d59a4c622cc138a6413d2e0fc0f95
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-29 15:20:54 +0400

    add models and migrations

--- kurs_service/contacts/models.py
+++ kurs_service/contacts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class DrivingDirection(models.Model):
+    img_line_metro = models.CharField(max_length=255, blank=True, null=True)
+    img_train = models.CharField(max_length=255, blank=True, null=True)
+    description_train = models.CharField(max_length=255, blank=True, null=True)
+    img_foot = models.CharField(max_length=255, blank=True, null=True)
+    description_foot = models.CharField(max_length=255, blank=True, null=True)
+    img_taxi = models.CharField(max_length=255, blank=True, null=True)
+    description_taxi = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Направление {self.id}"
+
+    class Meta:
+        db_table = "driving_directions"
+        
