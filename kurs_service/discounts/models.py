911b400713f8f8fe3c3499a7cb51cb106b14d32b
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-29 15:21:14 +0400

    add models and migrations

--- kurs_service/discounts/models.py
+++ kurs_service/discounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Discount(models.Model):
+    type = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    short_description = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    logo = models.CharField(max_length=255, blank=True, null=True)
+    img = models.CharField(max_length=255, blank=True, null=True)
+    date_start = models.CharField(max_length=255, blank=True, null=True)  # Можно заменить на DateField, если дата
+    date_stop = models.CharField(max_length=255, blank=True, null=True)   # будет в формате даты
+
+    def __str__(self):
+        return self.title if self.title else "Скидка"
+
+    class Meta:
+        db_table = "discounts"
