407549ee68178638d55f9e14f6514bc011b38b88
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-29 15:22:25 +0400

    add models and migrations

--- kurs_service/price_shod_razval/models.py
+++ kurs_service/price_shod_razval/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class ShodRazvalCategory(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    translit = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "shod_razval_category"
+        
+
+class ShodRazvalPrice(models.Model):
+    type = models.CharField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    sub_title = models.CharField(max_length=255, null=True, blank=True)
+    content = models.CharField(max_length=255, null=True, blank=True)
+    img = models.CharField(max_length=255, null=True, blank=True)
+    price = models.CharField(max_length=255, null=True, blank=True)
+    category_id = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+	
+    class Meta:
+        db_table = 'shod_razval_price'
+        
