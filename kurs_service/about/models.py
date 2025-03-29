0bf92441c60641704fd4c14288802d83aea191f6
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-29 15:20:28 +0400

    add models and migrations

--- kurs_service/about/models.py
+++ kurs_service/about/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class DescriptionCompany(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    short_description = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title if self.title else "Описание компании"
+
+    class Meta:
+        db_table = "description_company"
+        
+
+class Training(models.Model):
+    img = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        db_table = "training"
