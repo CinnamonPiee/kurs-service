8fab123fa9c9cb4e735b1434c64dfc482c900d1d
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-29 15:22:16 +0400

    add models and migrations

--- kurs_service/price_maintenance/models.py
+++ kurs_service/price_maintenance/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class TOCategory(models.Model):
+    parent_id = models.IntegerField(null=True, blank=True)  # Добавляем поле parent_id, которое может быть пустым
+    translate_category = models.CharField(max_length=255, blank=True, null=True)
+    title_category = models.CharField(max_length=255, blank=True, null=True)
+    img_category = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title_category
+
+    class Meta:
+        db_table = "t_o_category"
+
+
+class TOPrice(models.Model):
+    type = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    sub_title = models.CharField(max_length=255, blank=True, null=True)
+    content = models.CharField(max_length=255, blank=True, null=True)
+    img = models.CharField(max_length=255, blank=True, null=True)
+    price = models.CharField(max_length=255, blank=True, null=True)
+    category_id = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "t_o_price"
