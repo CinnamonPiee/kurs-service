ee4135f746719aac00571f61f94194ba1f97ef76
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-29 15:21:03 +0400

    add models and migrations

--- kurs_service/diagnostics/models.py
+++ kurs_service/diagnostics/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class DiagnosticsBlocksText(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description_text = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title if self.title else "Блок диагностики"
+
+    class Meta:
+        db_table = "diagnostics_blocks_text"
+
+
+class DiagnosticMethods(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    img = models.CharField(max_length=255, blank=True, null=True)
+    description_text = models.TextField(blank=True, null=True)
+    services_data = models.IntegerField(blank=True, null=True)
+    title_data_modal = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title if self.title else "Метод диагностики"
+
+    class Meta:
+        db_table = "diagnostic_methods"
