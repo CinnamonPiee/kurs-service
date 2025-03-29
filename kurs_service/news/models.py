2de5fa22134d9c50d836a1a3e6de1c153fdf9346
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-03-29 15:21:47 +0400

    add models and migrations

--- kurs_service/news/models.py
+++ kurs_service/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    short_description = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    logo = models.CharField(max_length=255, blank=True, null=True)
+    img = models.CharField(max_length=255, blank=True, null=True)
+    date_time = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title if self.title else "Новость"
+
+    class Meta:
+        db_table = "news"
+        
