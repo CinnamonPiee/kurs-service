d5b040de0c434ce32fc3439a5f57228b447f637a
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-01 21:44:36 +0400

    add new view on page

--- kurs_service/warranty/views.py
+++ kurs_service/warranty/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from warranty.models import (
+	WarrantyObligations,
+	WarrantyHeaders,
+)
 
 
 def warranty(request):
     context = {
         'title': 'Гарантия | OOO "КУРС"',
+        'warranty_text': WarrantyObligations.objects.all(),
+        'warranty_headers': WarrantyHeaders.objects.last(),
     }
     return render(request, "warranty/warranty.html", context=context)
