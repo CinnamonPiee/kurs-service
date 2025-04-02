ac3c499b22d480a1feacc0741013aba7ee143211
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-02 08:42:29 +0400

    add admin and url in app

--- kurs_service/rules_personal_data/views.py
+++ kurs_service/rules_personal_data/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def rules_personal_data(request):
+    context = {
+        'title': 'Правила обработки и хранения данных пользователя | OOO "КУРС"',
+    }
+    return render(request, "rules_personal_data/rules_personal_data.html", context=context)
