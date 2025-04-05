07e53be7612fcef8bf6218dc011e4b52ce66a74d
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-05 09:41:47 +0400

    add views in diagnostics page

--- kurs_service/diagnostics/views.py
+++ kurs_service/diagnostics/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from diagnostics.models import (
+	DiagnosticsMainDirections,
+	DiagnosticsWikiText,
+)
 
 
 def diagnostics(request):
     context = {
         'title': 'Диагностика | OOO "КУРС"',
+        'diagnostics_main_directions': DiagnosticsMainDirections.objects.all(),
+        'diagnostics_wiki_text': DiagnosticsWikiText.objects.all(),
     }
     return render(request, "diagnostics/diagnostics.html", context=context)
