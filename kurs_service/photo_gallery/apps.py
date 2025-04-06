b302a2d5e09fd7b242d4d650d19f94a315fc9a03
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-06 13:42:07 +0400

    add models in photo gallery page and change html conde with django

--- kurs_service/photo_gallery/apps.py
+++ kurs_service/photo_gallery/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PhotoGalleryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'photo_gallery'
+    verbose_name = 'Фотогалерея'
--- kurs_service/photo_gallery/views.py
+++ kurs_service/photo_gallery/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from photo_gallery.models import (
+    PhotoGalleryImages,
+    PhotoGalleryTitles,
+)
 
 
 def photo_gallery(request):
     context = {
         'title': 'Фото-галерея | OOO "КУРС"',
+        'photo_gallery_images': PhotoGalleryImages.objects.all(),
+        'photo_gallery_titles': PhotoGalleryTitles.objects.first(),
     }
     return render(request, "photo_gallery/photo_gallery.html", context=context)
