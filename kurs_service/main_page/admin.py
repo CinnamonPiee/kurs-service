7428873f3c33d7c3d6e1915933fb1208ead9397c
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-09 09:54:00 +0400

    add models in main page and input this models in html code

--- kurs_service/main_page/admin.py
+++ kurs_service/main_page/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from main_page.models import (
+	MainPageSlider,
+	MainPageDirectionOfActivity,
+	MainPageAlwaysInStock,
+	MainPageAdditionalPowerEquipmentAMTH,
+	MainPageAdditionalPowerEquipment,
+	MainPageOurAdvantages,
+	MainPageOurAdvantagesVideo,
+	MainPageQuestions,
+	MainPageEverythingToBoBest,
+	MainPageProfessionalEquipment,
+	MainPageTitles,
+)
 
-# Register your models here.
+
+@admin.register(MainPageSlider)
+class MainPageSliderAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageDirectionOfActivity)
+class MainPageDirectionOfActivityAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageAlwaysInStock)
+class MainPageAlwaysInStockAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageAdditionalPowerEquipmentAMTH)
+class MMainPageAdditionalPowerEquipmentAMTHAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageAdditionalPowerEquipment)
+class MainPageAdditionalPowerEquipmentAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageOurAdvantages)
+class MainPageOurAdvantagesAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageOurAdvantagesVideo)
+class MainPageOurAdvantagesVideoAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageQuestions)
+class MainPageQuestionsAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageEverythingToBoBest)
+class MainPageEverythingToBoBestAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageProfessionalEquipment)
+class MainPageProfessionalEquipmentAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(MainPageTitles)
+class MainPageTitlesAdmin(admin.ModelAdmin):
+	pass
