8be679bcdbe932cd4fead4d80e6ddd1eb4b4e741
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-11 16:58:47 +0400

    add new migrations

--- kurs_service/reviews/models.py
+++ kurs_service/reviews/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
 
-class ReviewsContent(models.Model):
-	pass
-	# TODO Доделать страницу отзывов после создания моделей отзывов и клиента
-
-
 class ReviewsTitles(models.Model):
 	visitor_reviews = models.CharField(max_length=255, null=True, blank=True, verbose_name="Отзывы посетителей")
 	leave_review_or_request = models.CharField(max_length=255, null=True, blank=True, verbose_name="Оставьте отзыв или пожелание")
