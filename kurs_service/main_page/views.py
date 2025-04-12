63369a8ea0e22c30239593caa8ccebc0326ac22a
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-12 19:47:12 +0400

    add urls and views for main page

--- kurs_service/main_page/views.py
+++ kurs_service/main_page/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def main_page(request):
+     context = {
+         'title': 'Главная страница | OOO "КУРС"',
+     }
+     return render(request, "main_page/main_page.html", context=context)
