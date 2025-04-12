9c655d8a1c56dd6ef87d70386c1d7d369cf5141a
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-12 19:46:54 +0400

    add verbose_name for base_page

--- kurs_service/base_page/apps.py
+++ kurs_service/base_page/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BasePageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base_page'
+    verbose_name = 'Базовая страница'
