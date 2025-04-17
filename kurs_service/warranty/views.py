4e8712723c220026eb1e0b494ce882bded42d2e9
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-17 14:11:51 +0400

    add views in warranty app

--- kurs_service/warranty/views.py
+++ kurs_service/warranty/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from warranty.models import (
+	WarrantySliderImages,
+	WarrantyObligations,
+	WarrantyTitles,
+)
 
-# Create your views here.
+
+def warranty(request):
+	context = {
+		'title': 'Гарантия | OOO "КУРС"',
+		'warranty_slider_images': WarrantySliderImages.objects.first(),
+		'warranty_obligations': WarrantyObligations.objects.all(),
+		'warranty_titles': WarrantyTitles.objects.first(),
+	}
+	return render(request, "warranty/warranty.html", context=context)
