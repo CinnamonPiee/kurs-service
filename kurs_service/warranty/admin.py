4d74492f6f26ee7eb0fd4ff43d8c04860f3448b2
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-04-17 11:33:03 +0400

    add admin models in warranty app

--- kurs_service/warranty/admin.py
+++ kurs_service/warranty/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from warranty.models import (
+	WarrantySliderImages,
+	WarrantyObligations,
+	WarrantyTitles,
+)
 
-# Register your models here.
+
+@admin.register(WarrantySliderImages)
+class WarrantySliderImagesAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(WarrantyObligations)
+class WarrantyObligationsAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(WarrantyTitles)
+class WarrantyTitlesAdmin(admin.ModelAdmin):
+	pass
