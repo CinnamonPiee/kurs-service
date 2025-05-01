82cbec4515688866a9c1565aea86637baec6b244
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-05-01 10:43:18 +0400

    add user model and create base hash password for user. add model in django admin. in html code add functionals what if user is auth then site show his email and button to exit

--- kurs_service/base_page/admin.py
+++ kurs_service/base_page/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SiteVisitor
 
-# Register your models here.
+
+@admin.register(SiteVisitor)
+class SiteVisitorAdmin(admin.ModelAdmin):
+	pass
--- kurs_service/base_page/models.py
+++ kurs_service/base_page/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your models here.
+
+class SiteVisitor(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    last_name = models.CharField(max_length=50, verbose_name="Фамилия", blank=True, null=True)
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
+    phone = models.CharField(max_length=15, verbose_name="Телефон", blank=True, null=True)
+    password = models.CharField(max_length=128, verbose_name="Пароль")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата регистрации")
+    last_login = models.DateTimeField(auto_now_add=True, verbose_name="Последний вход")
+    is_frequent = models.BooleanField(default=False, verbose_name="Частый клиент")
+    visit_count = models.PositiveIntegerField(default=0, verbose_name="Количество посещений")
+    notes = models.TextField(blank=True, null=True, verbose_name="Заметки")
+
+    def set_password(self, raw_password):
+        """Устанавливает хэшированный пароль."""
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        """Проверяет, соответствует ли пароль хэшу."""
+        return check_password(raw_password, self.password)
+
+    def __str__(self):
+        return f"{self.first_name + " " + self.last_name}" if self.first_name else self.email
+
+    class Meta:
+        db_table = "base_page_site_visitor"
+        verbose_name = "Посетитель сайта"
+        verbose_name_plural = "Посетители сайта"
