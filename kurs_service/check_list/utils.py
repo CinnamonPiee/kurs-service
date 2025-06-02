c04769f2ddb5ac45a523d0c3333f8dc7bd009b70
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-06-02 09:30:32 +0400

    add utils for form in check_list app

--- kurs_service/check_list/utils.py
+++ kurs_service/check_list/utils.py
@@ -3,18 +3,15 @@ from .models import CheckListOrderItem
 
 
 def get_session_cart(request):
-    """Получить корзину из сессии пользователя."""
     return request.session.get("check_list", {})
 
 
 def save_session_cart(request, cart):
-    """Сохранить корзину в сессию пользователя."""
     request.session["check_list"] = cart
     request.session.modified = True
 
 
 def add_to_cart(request, service_id, service_name, img_url):
-    """Добавить услугу в корзину."""
     cart = get_session_cart(request)
     if str(service_id) in cart:
         cart[str(service_id)]["quantity"] += 1
@@ -29,7 +26,6 @@ def add_to_cart(request, service_id, service_name, img_url):
 
 
 def increment_cart_item(request, service_id):
-    """Увеличить количество услуги в корзине."""
     cart = get_session_cart(request)
     if str(service_id) in cart:
         cart[str(service_id)]["quantity"] += 1
@@ -37,7 +33,6 @@ def increment_cart_item(request, service_id):
 
 
 def decrement_cart_item(request, service_id):
-    """Уменьшить количество услуги в корзине."""
     cart = get_session_cart(request)
     if str(service_id) in cart:
         cart[str(service_id)]["quantity"] -= 1
@@ -47,7 +42,6 @@ def decrement_cart_item(request, service_id):
 
 
 def remove_cart_item(request, service_id):
-    """Удалить услугу из корзины."""
     cart = get_session_cart(request)
     if str(service_id) in cart:
         del cart[str(service_id)]
@@ -55,12 +49,10 @@ def remove_cart_item(request, service_id):
 
 
 def get_cart_items(request):
-    """Получить все позиции корзины."""
     cart = get_session_cart(request)
     return [v for v in cart.values()]
 
 
 def get_cart_count(request):
-    """Получить общее количество услуг в корзине."""
     cart = get_session_cart(request)
     return sum(item["quantity"] for item in cart.values())
